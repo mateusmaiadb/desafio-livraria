24ecee8093accfb93e7a76dc8adcb8712712339d
Mateus Pereira <mateus.maia@dbserver.com.br>
2023-07-28 12:10:32 -0300

    first commit

---
+++ src/test/java/com/db/apilivraria/ApiLivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.apilivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
