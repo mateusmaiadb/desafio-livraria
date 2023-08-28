fa258cfb00a922985c279cbb9572de3bf42772dd
Mateus Pereira <mateus.maia@dbserver.com.br>
2023-08-28 15:17:05 -0300

    test: mais cenários de testes unitarios

--- src/test/java/com/db/apilivraria/services/LivroServiceTest.java
+++ src/test/java/com/db/apilivraria/services/LivroServiceTest.java
@@ -1,7 +1,10 @@
 package com.db.apilivraria.services;
 
+import com.db.apilivraria.dtos.AutorDto;
 import com.db.apilivraria.dtos.LivroDto;
+import com.db.apilivraria.models.Autor;
 import com.db.apilivraria.models.Livro;
+import com.db.apilivraria.repositories.AutorRepository;
 import com.db.apilivraria.repositories.LivroRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +14,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +28,12 @@ class LivroServiceTest {
 
     @Mock
     private LivroRepository livroRepository;
+
+    @Mock
+    private AutorRepository autorRepository;
+
+    @Mock
+    AutorService autorService;
 
     @InjectMocks
     private LivroService livroService;
@@ -50,7 +61,6 @@ class LivroServiceTest {
         livroDto.setId(1L);
         livroDto.setNome("Alice no país das maravilhas");
         livroDto.setIsbn("1254787878");
-
     }
 
     @Test
@@ -113,5 +123,77 @@ class LivroServiceTest {
         livroService.excluirlivro(livro.getId());
 
         verify(livroRepository, times(1)).delete(livro);
+    }
+
+    @Test
+    @DisplayName("Criar livro com autores existentes")
+    void criarLivroWithExistingAutores() {
+        LivroDto livroDto = new LivroDto();
+        livroDto.setNome("Livro Teste");
+        livroDto.setIsbn("1234567890");
+
+        AutorDto autorDto1 = new AutorDto();
+        autorDto1.setNome("Autor1");
+        autorDto1.setCpf("11111111111");
+
+        AutorDto autorDto2 = new AutorDto();
+        autorDto2.setNome("Autor2");
+        autorDto2.setCpf("22222222222");
+
+        livroDto.setAutores(Arrays.asList(autorDto1, autorDto2));
+
+        Autor autorExistente1 = new Autor();
+        autorExistente1.setId(1L);
+        autorExistente1.setNome("Autor1");
+        autorExistente1.setCpf("11111111111");
+
+        Autor autorExistente2 = new Autor();
+        autorExistente2.setId(2L);
+        autorExistente2.setNome("Autor2");
+        autorExistente2.setCpf("22222222222");
+
+        when(autorRepository.findByCpf(autorDto1.getCpf())).thenReturn(autorExistente1);
+        when(autorRepository.findByCpf(autorDto2.getCpf())).thenReturn(autorExistente2);
+
+        Livro livroSalvo = new Livro();
+        livroSalvo.setId(1L);
+        livroSalvo.setNome("Livro Teste");
+        livroSalvo.setIsbn("1234567890");
+        livroSalvo.setAutores(Arrays.asList(autorExistente1, autorExistente2));
+
+        when(livroRepository.save(any(Livro.class))).thenReturn(livroSalvo);
+
+        LivroDto livroCriado = livroService.criarLivro(livroDto);
+
+        assertEquals("Livro Teste", livroCriado.getNome());
+        assertEquals("1234567890", livroCriado.getIsbn());
+        assertEquals(2, livroCriado.getAutores().size());
+        assertEquals("Autor1", livroCriado.getAutores().get(0).getNome());
+        assertEquals("Autor2", livroCriado.getAutores().get(1).getNome());
+    }
+
+    @Test
+    @DisplayName("Excluir livro por ID não encontrado")
+    void excluirLivroByIdNotFound() {
+        Long livroId = 1L;
+
+        when(livroRepository.findById(livroId)).thenReturn(Optional.empty());
+
+        assertThrows(IllegalArgumentException.class, () -> livroService.excluirlivro(livroId));
+        verify(livroRepository, never()).delete(any(Livro.class));
+    }
+
+    @Test
+    @DisplayName("Atualizar livro por ID não encontrado")
+    void atualizarLivroByIdNotFound() {
+        Long livroId = 1L;
+        LivroDto livroDto = new LivroDto();
+        livroDto.setNome("Livro Atualizado");
+        livroDto.setIsbn("9876543210");
+
+        when(livroRepository.findById(livroId)).thenReturn(Optional.empty());
+
+        assertThrows(IllegalArgumentException.class, () -> livroService.atualizarLivro(livroId, livroDto));
+        verify(livroRepository, never()).save(any(Livro.class));
     }
 }
